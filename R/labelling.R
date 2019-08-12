2e529dca543dd1050004f63e7785c15bd8f5438f
Bangyou Zheng <zhe00a@caps-rstudio-vc.it.csiro.au>
2019-08-12 16:02:16 +1000

    Add API for labelling of images

--- R/labelling.R
+++ R/labelling.R
@@ -19,3 +19,23 @@ get_labelling <- function(id) {
     response <- httr::content(response)
     response
 }
+
+#' Get labelling of images for a flight
+#'
+#' @param id The flight id
+#'
+#' @return A list with labelling
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' pc_login()
+#' get_flight_labelling_image(1)
+#' }
+get_labelling_image <- function(flight) {
+    .check_id(flight)
+    response <- request(httr::GET, paste0('flight/', flight, '/labelling/image'))
+    httr::stop_for_status(response)
+    response <- httr::content(response)
+    response
+}
